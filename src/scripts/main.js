c7e6bfa71f3ebfdfdafcf80a3f2db1d3f69bbc1c
Arseniy Naumov <ivanov262ivanov@gmail.com>
2025-10-06 16:34:21 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -21,7 +21,7 @@ const pushNotification = (posTop, posRight, title, description, type) => {
 
   notification.setAttribute(
     'style',
-    ` top: ${posTop}px; right: ${posRight}px;`,
+    `position: fixed; top: ${posTop}px; right: ${posRight}px;`,
   );
 
   setTimeout(() => {
